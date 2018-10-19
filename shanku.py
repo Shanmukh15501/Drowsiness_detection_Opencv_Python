72b638ee6e8190137ebb85cdc77b10b948c6a46f
Shanmukh Adari <34175481+Shanmukh15501@users.noreply.github.com>
2018-10-19 08:02:20 +0530

    Add files via upload

---
+++ shanku.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+from threading import *
+import playsound
+import time
+
+def Level2():
+    playsound.playsound("Beep.wav")
+eyesOpen = True
+global count
+count = 0
+global c
+c=round(time.time())
+face_cascade=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml') 
+Video = cv2.VideoCapture(0)
+time.sleep(3)
+def eye(eyes):
+    tuple_size=len(eyes)
+    if(tuple_size!=0):
+        for (a,b,c,d) in eyes:
+            cv2.rectangle(frame,(a,b),(a+c,b+d),(0,127,255),2)
+    else:
+        p = Thread(target = Level2)
+        p.start()
+def face(faces):
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        #roi_face = frame[y:y+h,x:x+w]
+        
+while True:
+    check,frame=Video.read()
+    eyes=eye_cascade.detectMultiScale(frame,1.3,5)
+    faces=face_cascade.detectMultiScale(frame)
+    face(faces)
+    eye(eyes) 
+    cv2.imshow('Captures',frame)
+    key=cv2.waitKey(1)
+    if key == ord('q'):
+        break
+Video.release() 
+cv2.destroyAllWindows()
