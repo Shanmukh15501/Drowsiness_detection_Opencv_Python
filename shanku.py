d69b21bb233f98de113d33b4e9d3aecd3e0e973e
Shanmukh Adari <34175481+Shanmukh15501@users.noreply.github.com>
2018-10-22 02:51:09 +0530

    Update shanku.py

--- shanku.py
+++ shanku.py
@@ -4,31 +4,53 @@ from threading import *
 import playsound
 import time
 
+
+ct=0
+
 def Level2():
     playsound.playsound("Beep.wav")
-    
+
 face_cascade=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml') 
 Video = cv2.VideoCapture(0)
 time.sleep(3)
+
 def eye(eyes):
-    tuple_size=len(eyes)
-    if(tuple_size!=0):
+    global ct
+    dimensions=len(eyes)
+    if(dimensions!=0):
         for (a,b,c,d) in eyes:
             cv2.rectangle(frame,(a,b),(a+c,b+d),(0,127,255),2)
+            ct=0
+            
     else:
-        p = Thread(target = Level2)
-        p.start()
+        count= ct
+        count=count+1
+        ct=count
+        print("counts in",count)
+        if count==5:
+            print("Rings in count",count)  
+            count=0
+            ct=0
+            p = Thread(target = Level2)
+            p.start()
+
 def face(faces):
-    for (x,y,w,h) in faces:
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-        
+    dimension=len(faces)
+    if (dimension!=0):
+        for (x,y,w,h) in faces:
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+            eye(eyes)
+    else:
+        pass
+
+            
 while True:
     check,frame=Video.read()
     eyes=eye_cascade.detectMultiScale(frame,1.3,5)
+
     faces=face_cascade.detectMultiScale(frame)
     face(faces)
-    eye(eyes) 
     cv2.imshow('Captures',frame)
     key=cv2.waitKey(1)
     if key == ord('q'):
