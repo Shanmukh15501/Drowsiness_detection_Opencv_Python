cd4e926ce903db35eb0f8d44a66d175c4c61eb90
Shanmukh Adari <34175481+Shanmukh15501@users.noreply.github.com>
2019-03-12 11:30:09 +0530

    Update datasettrainer.py

--- datasettrainer.py
+++ datasettrainer.py
@@ -1,5 +1,3 @@
-
-
 import cv2
 import numpy as np
 import os
@@ -28,30 +26,24 @@ else:
     print ("Successfully created the directory ")
 
 global sample
-
 sample=0
-
-
 print("IN")
 
-while True:
-    check,frame=Video.read()
-    faces=face_cascade.detectMultiScale(frame,scaleFactor=3.0)
-    for (x,y,w,h) in faces:
-      
+while sample!=-1:
+   check,frame=Video.read()
+   faces=face_cascade.detectMultiScale(frame,scaleFactor=1.05,minNeighbors=5)
+   for (x,y,w,h) in faces:
         sample=sample+1
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
         op=frame[y:y+h,x:x+w]
         cv2.imwrite(str(folder)+" " + str(id) + " " + str(sample) +  ".jpg",op)
         print("THE VALUE IS",sample)
-        cv2.imshow('Captures',frame)
-    if(sample==500):
-        break
-    key=cv2.waitKey(1)
-    #if key == ord('q'):
-     #  break
-
-Video.release() 
+        cv2.imshow('Captures',frame) 
+        key=cv2.waitKey(2)#updates for every 2 milliseconds
+        if sample==100:
+            sample=-1
+            break
+Video.release()  
 cv2.destroyAllWindows()
 for i in os.listdir(path):
-    print(i)
+    print(i)   
